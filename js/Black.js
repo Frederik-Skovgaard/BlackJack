1b2b209a4166f0d532ca5f4ecd07c0fbbb28fbe3
Frederik Skovgaard <frederik.skovgaard@gmail.com>
2021-09-17 09:24:14 +0200

    Base layout

--- js/Black.js
+++ js/Black.js
@@ -1,6 +1,49 @@
+//---------------Varibles---------------
 let GameStart = false;
 let gameOver = false;
 let playerWon = false;
 
+let dealersCard =  ["wow", "wow"];
+let playersCard = [""];
+
 let dealerScore = 0;
 let playerScore = 0;
+
+//---------------Function---------------
+
+
+function createCardGame(){
+
+}
+
+function mixGameCards(gameCards) {
+
+}
+
+function getCardString(card) {
+
+}
+
+function nextCard() {
+
+}
+
+function getValueOfCard(card) {
+
+}
+
+function getScore(card) {
+
+}
+
+function updateScore() {
+
+}
+
+function chekcIfGameIsOver() {
+
+}
+
+function viewStatus() {
+    
+}
